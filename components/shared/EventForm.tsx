acacc159703f1a05f0e41f9b7cf10436def264b7
UnicornDad8 <unicorndad8@gmail.com>
2024-08-05 10:13:13 -0400

    Added event actions createEvent function and finishing onSubmit form on EventForm component and redirecting to event details page

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -14,6 +14,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { eventFormSchema } from "@/lib/validator";
 import { eventDefaultValues } from "@/constants";
@@ -22,7 +23,9 @@ import { Textarea } from "@/components/ui/textarea";
 import { FileUploader } from "./FileUploader";
 import { Checkbox } from "@/components/ui/checkbox";
 import DatePicker from "react-datepicker";
+import { useUploadThing } from "@/lib/uploadthing";
 import Image from "next/image";
+import { createEvent } from "@/lib/actions/event.actions";
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -35,16 +38,45 @@ const EventForm = ({ userId, type }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([]);
   const initialValues = eventDefaultValues;
 
+  const router = useRouter();
+
+  const { startUpload } = useUploadThing("imageUploader");
+
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
     defaultValues: initialValues,
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof eventFormSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    let uploadedImageUrl = values.imageUrl;
+
+    if (files.length > 0) {
+      const uploadedImages = await startUpload(files);
+
+      if (!uploadedImages) {
+        return;
+      }
+
+      uploadedImageUrl = uploadedImages[0].url;
+    }
+
+    if (type === "Create") {
+      try {
+        const newEvent = await createEvent({
+          event: { ...values, imageUrl: uploadedImageUrl },
+          userId,
+          path: "/profile",
+        });
+
+        if (newEvent) {
+          form.reset();
+          router.push(`/events/${newEvent._id}`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 
   return (
---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+
+import { connectToDatabase } from "@/lib/database";
+import Event from "@/lib/database/models/event.model";
+import User from "@/lib/database/models/user.model";
+import { handleError } from "@/lib/utils";
+
+import { CreateEventParams } from "@/types";
+
+// CREATE
+export async function createEvent({ userId, event, path }: CreateEventParams) {
+  try {
+    await connectToDatabase();
+
+    const organizer = await User.findById(userId);
+    if (!organizer) throw new Error("Organizer not found");
+
+    const newEvent = await Event.create({
+      ...event,
+      category: event.categoryId,
+      organizer: userId,
+    });
+    revalidatePath(path);
+
+    return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+}
