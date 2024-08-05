365198839a17c3d66354a0b04c65e1e6dce81b29
UnicornDad8 <unicorndad8@gmail.com>
2024-08-05 13:11:38 -0400

    Added populateEvent to event.actions.ts file and getEventById before displaying on EventDetails page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,9 +5,20 @@ import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "@/lib/database";
 import Event from "@/lib/database/models/event.model";
 import User from "@/lib/database/models/user.model";
+import Category from "@/lib/database/models/category.model";
 import { handleError } from "@/lib/utils";
 
 import { CreateEventParams } from "@/types";
+
+const populateEvent = (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 // CREATE
 export async function createEvent({ userId, event, path }: CreateEventParams) {
@@ -25,6 +36,21 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     revalidatePath(path);
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// GET ONE EVENT BY ID
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) throw new Error("Event not found");
+
+    return JSON.parse(JSON.stringify(event));
   } catch (error) {
     handleError(error);
   }
