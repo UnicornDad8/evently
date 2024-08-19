55e262c09b37a40a5055ad469bf59b8a87765037
UnicornDad8 <unicorndad8@gmail.com>
2024-08-19 13:34:12 -0400

    Started Checkout Button and removed vercel deploy

---
+++ components/shared/CheckoutButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { IEvent } from "@/lib/database/models/event.model";
+import { useUser } from "@clerk/nextjs";
+import React from "react";
+
+const CheckoutButton = ({ event }: { event: IEvent }) => {
+  const { user } = useUser();
+  const userId = user?.publicMetadata.userId as string;
+  const hasEventFinished = new Date(event.endDateTime) < new Date();
+
+  return <div className="flex items-center gap-3">Checkout Button</div>;
+};
+
+export default CheckoutButton;
