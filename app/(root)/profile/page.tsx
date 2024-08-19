33bd0c497077d3d681d018a632ad69a1714bc126
UnicornDad8 <unicorndad8@gmail.com>
2024-08-19 13:13:54 -0400

    Fetching events organized by user

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { getEventsByUser } from "@/lib/actions/event.actions";
 import Link from "next/link";
 import Collection from "@/components/shared/Collection";
+import { auth } from "@clerk/nextjs";
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const organizedEvents = await getEventsByUser({ userId, page: 1 });
+
   return (
     <>
       {/* ------- My Tickets -------- */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
-          <Button asChild className="button hidden sm:flex">
+          <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/#events">Explore More Events</Link>
           </Button>
         </div>
@@ -31,14 +38,14 @@ const ProfilePage = () => {
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
-          <Button asChild className="button hidden sm:flex">
+          <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/events/create">Create New Event</Link>
           </Button>
         </div>
       </section>
-      {/*<section className="wrapper my-8">
+      <section className="wrapper my-8">
         <Collection
-          data={events?.data}
+          data={organizedEvents?.data}
           emptyTitle="No events have been created yet"
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
@@ -47,7 +54,7 @@ const ProfilePage = () => {
           urlParamName="eventsPage"
           totalPages={2}
         />
-      </section>*/}
+      </section>
     </>
   );
 };
